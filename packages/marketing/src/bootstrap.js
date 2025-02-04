35c9f11859eba5146c6dc398c6943471b687cfce
akrishna4 <akrishna@sonicwall.com>
2025-02-04 21:21:34 +0530

    Initial Commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+
+const mount = (el) => {
+    ReactDOM.render(<App />, el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.getElementById("_marketing-dev-root");
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+export { mount };
