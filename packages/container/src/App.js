3cc12746ef7effdd7409c5bd2076ff54cf0e63d7
akrishna4 <akrishna@sonicwall.com>
2025-02-06 00:37:05 +0530

    added invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return <>
-       Hi there!!!
+       Hi there!!!!
        <hr/>
         <MarketingApp/>
     </>;
