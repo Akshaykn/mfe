aa59e1916616c2ffe4b4ac6eb85c088f53aed790
akrishna4 <akrishna@sonicwall.com>
2025-02-09 13:45:25 +0530

    added the header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
-    return <>
-       Hi there!!!!
-       <hr/>
+    return (<BrowserRouter>
+     <div>
+        <Header/>
         <MarketingApp/>
-    </>;
+    </div>
+    </BrowserRouter>);
 }
